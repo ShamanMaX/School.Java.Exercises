591ac4ac479acd8a64076aa9f76e5b5e41d1de6a
Mad_Scientist_V3 <Mad_Scientist_V3@Mad_Machine_V3>
2016-11-13 21:31:18 +0100

    Zad3Cw7

---
+++ Zad3Cw7/src/zad3cw7/Zad3Cw7.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zad3cw7;
+import java.util.Scanner;
+/**
+ *
+ * @author Mad_Scientist_V3
+ */
+public class Zad3Cw7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner zxc = new Scanner (System.in);   
+        System.out.println("Podaj znak wypelnienia:");
+        char z = zxc.nextChar();
+        
+        
+    }
+    
+}
